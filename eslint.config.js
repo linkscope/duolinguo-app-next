566f602024d96003d8f10d1b36df47e57cb80841
linkscope <linkscope@foxmail.com>
2024-05-07 00:45:05 +0800

    chore: unocss config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc'
+import unocss from '@unocss/eslint-config/flat'
 
 const compat = new FlatCompat()
 
-export default [...compat.extends('next/core-web-vitals', 'alloy', 'alloy/react', 'alloy/typescript')]
+export default [...compat.extends('next/core-web-vitals', 'alloy', 'alloy/react', 'alloy/typescript'), unocss]
