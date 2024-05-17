a57f3c1624071d891c606cf5c1351fb1b00f3f56
linkscope <linkscope@foxmail.com>
2024-05-18 00:46:08 +0800

    chore(db/queries): queries update

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from 'react'
 import db from './drizzle'
 import { auth } from '@clerk/nextjs/server'
 import { eq } from 'drizzle-orm'
-import { challengeProgress, courses, units, userProgress } from './schema'
+import { challengeProgress, courses, lessons, units, userProgress } from './schema'
 
 // 获取课程列表
 export const getCourses = cache(async () => db.query.courses.findMany())
@@ -72,4 +72,105 @@ export const getUserProgress = cache(async () => {
       activeCourse: true,
     },
   })
+})
+
+// 获取用户第一个还没完成的课程
+export const getCourseProgress = cache(async () => {
+  const { userId } = auth()
+  const userProgress = await getUserProgress()
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return null
+  }
+
+  const unitsInActiveCourse = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(units.order)],
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
+        with: {
+          unit: true,
+          challenges: {
+            with: {
+              challengeProgresses: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+
+  return unitsInActiveCourse
+    .flatMap((unit) => unit.lessons)
+    .find((lesson) => {
+      return lesson.challenges.some(
+        (challenge) =>
+          !challenge.challengeProgresses ||
+          challenge.challengeProgresses.length === 0 ||
+          challenge.challengeProgresses.some((progress) => progress.completed === false),
+      )
+    })
+})
+
+// 获取课程列表
+export const getLesson = cache(async (id?: number) => {
+  const { userId } = auth()
+  if (!userId) {
+    return null
+  }
+
+  const courseProgress = await getCourseProgress()
+  const lessonId = id || courseProgress?.id
+  if (!lessonId) {
+    return null
+  }
+
+  const data = await db.query.lessons.findFirst({
+    where: eq(lessons.id, lessonId),
+    with: {
+      challenges: {
+        orderBy: (challenges, { asc }) => [asc(challenges.order)],
+        with: {
+          challengeOptions: true,
+          challengeProgresses: {
+            where: eq(challengeProgress.userId, userId),
+          },
+        },
+      },
+    },
+  })
+
+  if (!data || !data.challenges) {
+    return null
+  }
+
+  const normalizedChallenges = data.challenges.map((challenge) => {
+    const completed =
+      challenge.challengeProgresses &&
+      challenge.challengeProgresses.length > 0 &&
+      challenge.challengeProgresses.every((progress) => progress.completed)
+
+    return { ...challenge, completed }
+  })
+
+  return { ...data, challenges: normalizedChallenges }
+})
+
+// 获取用户当前课程进度
+export const getLessonPercentage = cache(async () => {
+  const courseProgress = await getCourseProgress()
+  if (!courseProgress?.id) {
+    return 0
+  }
+
+  const lesson = await getLesson(courseProgress.id)
+  if (!lesson) {
+    return 0
+  }
+
+  const completedChallenges = lesson.challenges.filter((challenge) => challenge.completed)
+  return Math.round((completedChallenges.length / lesson.challenges.length) * 100)
 })
--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -3,13 +3,23 @@ import FeedWrapper from '@/components/feed-wrapper'
 import LearnHeader from '@/app/(main)/learn/components/header'
 import LearnUnit from '@/app/(main)/learn/components/unit'
 import UserProgress from '@/components/user-progress'
-import { getUnits, getUserProgress } from '@/database'
+import type { lessons, units as unitsSchema } from '@/database'
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from '@/database'
 import { redirect } from 'next/navigation'
 
 export default async function LearnPage() {
-  const [userProgress, units] = await Promise.all([getUserProgress(), getUnits()])
+  const [userProgress, units, courseProgress, lessonPercentage] = await Promise.all([
+    getUserProgress(),
+    getUnits(),
+    getCourseProgress(),
+    getLessonPercentage(),
+  ])
 
   if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses')
+  }
+
+  if (!courseProgress) {
     redirect('/courses')
   }
 
@@ -27,7 +37,17 @@ export default async function LearnPage() {
         <LearnHeader title={userProgress.activeCourse.title} />
         {units.map((unit) => (
           <div key={unit.id} className="mb-10">
-            <LearnUnit unit={unit} activeLesson={undefined} activeLessonPercentage={0} />
+            <LearnUnit
+              unit={unit}
+              activeLesson={
+                courseProgress as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect
+                    })
+                  | undefined
+              }
+              activeLessonPercentage={lessonPercentage}
+            />
           </div>
         ))}
       </FeedWrapper>
